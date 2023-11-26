6093699a8a26ce41833b23829bf296da45690b3b
frank campos <frankcampos85@gmail.com>
2023-11-26 12:16:27 -0600

    now I can add new Authors, however, I can not delete or edit them

--- events/formEvents.js
+++ events/formEvents.js
@@ -1,37 +1,54 @@
-import { getAuthors, updateAuthor } from '../api/authorData';
+import { createAuthor, getAuthors, updateAuthor } from '../api/authorData';
 import { showAuthors } from '../pages/authors';
 
 const formEvents = () => {
   document.querySelector('#main-container').addEventListener('submit', (e) => {
     e.preventDefault();
-    // TODO: CLICK EVENT FOR SUBMITTING FORM FOR ADDING A BOOK
+
+    // Handling submission for adding a book
     if (e.target.id.includes('submit-book')) {
       console.warn('CLICKED SUBMIT BOOK', e.target.id);
     }
 
-    // TODO: CLICK EVENT FOR EDITING A BOOK
+    // Handling submission for updating a book
     if (e.target.id.includes('update-book')) {
       const [, firebaseKey] = e.target.id.split('--');
       console.warn('CLICKED UPDATE BOOK', e.target.id);
       console.warn(firebaseKey);
     }
 
-    // FIXME: ADD CLICK EVENT FOR SUBMITTING FORM FOR ADDING AN AUTHOR
+    // Handling submission for adding an author
     if (e.target.id.includes('submit-author')) {
       console.warn('CLICKED SUBMIT AUTHOR');
+      const payload = {
+        email: document.querySelector('#email').value,
+        first_name: document.querySelector('#first_name').value,
+        last_name: document.querySelector('#last_name').value,
+      };
+
+      createAuthor(payload)
+        .then((data) => {
+          console.warn(data);
+          return getAuthors(); // Call getAuthors after logging the data
+        })
+        .then(showAuthors)
+        .catch((error) => {
+          console.error('An error occurred:', error);
+        });
     }
-    // FIXME:ADD CLICK EVENT FOR EDITING AN AUTHOR
-    if (e.target.id.includes('update-author')) {
+
+    // Handling submission for updating an author
+    if (e.target.id.includes('update-author-form')) {
       console.warn('clicked update author');
       const [, firebaseKey] = e.target.id.split('--');
 
-      const payload = {
+      const payload2 = {
         first_name: document.querySelector('#first_name').value,
         last_name: document.querySelector('#last_name').value,
         email: document.querySelector('#email').value,
         firebaseKey
       };
-      updateAuthor(payload).then(() => {
+      updateAuthor(payload2).then(() => {
         getAuthors().then(showAuthors);
       });
     }
